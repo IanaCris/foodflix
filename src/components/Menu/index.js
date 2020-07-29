42f0606cc657596135f5e6b59d18ae9e785e2b30
IanaCris <ianasousati@gmail.com>
2020-07-29 19:09:33 -0300

    cadastro de categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
-//import ButtonLink from './components/ButtonLink';
 import { Link } from 'react-router-dom';
+//import ButtonLink from './components/ButtonLink';
 
 export default function Menu() {
   return (
     <nav className="Menu">
       <Link to="/">
-        <img className="Logo" src={Logo} alt="IanaFlix" />
+        <img className="Logo" src={Logo} alt="FoodFlix" />
       </Link>
 
       <Button as={Link} className="ButtonLink" to="/cadastro/video">
